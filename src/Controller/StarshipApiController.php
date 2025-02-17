2c8d890e172ba40ef5cade5f6c84f2d5038414e0
DavidGomezGonzalez <gomezdavid234@gmail.com>
2025-02-17 19:07:34 +0100

    Rutas más sofisticadas
    Rutas más sofisticadas:
    Requisitos, comodinos y más

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,15 +9,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('', methods: ['GET'])]
     public function getCollection(StarshipRepository $respository): Response
     {
         //dd($logger);
         //$logger->info('Starship collection retrived');
         //dd($respository);
         $starships = $respository->findAll();
+        return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $respository): Response {
+        $starships = $respository->find($id);
+
+        if(!$starships) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
         return $this->json($starships);
     }
 }
