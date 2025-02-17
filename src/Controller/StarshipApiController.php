14fac45fb6c776af2e7cf05fbe95b91405f08b50
DavidGomezGonzalez <gomezdavid234@gmail.com>
2025-02-17 18:31:27 +0100

    Servicio Propip
    Crear tu propio servicio

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use App\Repository\StarshipRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,33 +12,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(LoggerInterface $logger): Response
+    public function getCollection(StarshipRepository $respository): Response
     {
         //dd($logger);
-        $logger->info('Starship collection retrived');
-        $starships = [
-            new Starship(
-                1,
-                'USS LeafyCruiser (NCC-0001)',
-                'Garden',
-                'Jean-Luc Pickles',
-                'taken over by Q'
-            ),
-            new Starship(
-                2,
-                'USS Espresso (NCC-1234-C)',
-                'Latte',
-                'James T. Quick!',
-                'repaired',
-            ),
-            new Starship(
-                3,
-                'USS Wanderlust (NCC-2024-W)',
-                'Delta Tourist',
-                'Kathryn Journeyway',
-                'under construction',
-            ),
-        ];
+        //$logger->info('Starship collection retrived');
+        //dd($respository);
+        $starships = $respository->findAll();
         return $this->json($starships);
     }
 }
